646e489a35f00eef74b7b676142455f034d867d6
Benchmark <53467586+yanivbahalul@users.noreply.github.com>
2025-05-03 22:02:37 +0300

    fix anti cheat

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -119,7 +119,7 @@ namespace HelloWorldWeb.Pages
             var rapidTotal = HttpContext.Session.GetInt32("RapidTotal") ?? 0;
             var rapidCorrect = HttpContext.Session.GetInt32("RapidCorrect") ?? 0;
 
-            if (elapsedSeconds <= 60)
+            if (elapsedSeconds <= 120)
             {
                 HttpContext.Session.SetInt32("RapidTotal", rapidTotal + 1);
                 if (IsCorrect)
